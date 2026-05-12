9dd46e5e7312550f88d545c7cf66e0aaa4388046
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-05-12 20:44:34 +0300

    save function

---
+++ src/api/studentApi.js
@@ -0,0 +1,57 @@
+const BASE_URL = 'http://localhost:3000/students';
+
+
+
+export const getStudents = async () => {
+    const res = await fetch((`${BASE_URL}`))
+    return res.json()
+}
+
+
+
+
+
+
+//створення нового студента
+
+
+export const addStudent = async (studentData) => {
+    const options = {
+        method: "POST", // створення
+        body: JSON.stringify(studentData), // Дані, які ми відправляємо
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }
+        const result = await fetch(BASE_URL, options)
+        return result.json()
+}
+
+
+
+// оновлення даних
+
+
+export const updateStudent = async(id, updateData) => {
+    const options = {
+        method: "PATCH", 
+        body: JSON.stringify(updatedData),
+        headers: { "Content-Type": "application/json; charset=UTF-8" },
+    };
+    const res = await fetch(`${BASE_URL}/${id}`, options);
+    return res.json();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
